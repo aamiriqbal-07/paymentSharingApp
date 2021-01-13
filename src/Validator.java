36bb13b7a3a0baab517a740faab0b6d2714ab021
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-13 06:46:24 +0530

    Add phone length validity checker

--- src/Validator.java
+++ src/Validator.java
@@ -2,9 +2,15 @@ import java.util.HashMap;
 
 public class Validator {
 
+    public static Boolean phoneValidity(String phone) {
+        if(phone.length() == 10)  return true;
+        return false;
+    }
+
     public static Boolean checkRegistry( HashMap<Integer, User> registeredUser, User user ) {
-        for(int i = 1; i<= registeredUser.size() ; i++) {
-            if( user.getPhone() == registeredUser.get(i).getPhone() ) return false;
+
+        for (int i = 1; i <= registeredUser.size(); i++) {
+            if (user.getPhone() == registeredUser.get(i).getPhone()) return false;
         }
         return true;
     }
