fe81aa1097ef17ca68003a2a97e64366a2a5c41f
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-13 06:49:11 +0530

    Update for users phone and registry verification

--- src/NewRegister.java
+++ src/NewRegister.java
@@ -5,6 +5,10 @@ public class NewRegister {
     static private Integer idCount = 0;
     static HashMap<Integer, User> registeredUser = new HashMap<Integer, User>();
     public static String Register(User user) {
+        if( !Validator.phoneValidity(user.getPhone()) ) {
+            return "Invalid Phone!";
+        }
+
         Boolean validate = Validator.checkRegistry( registeredUser, user );
         if(validate) {
             NewRegister.idCount++;
@@ -12,5 +16,21 @@ public class NewRegister {
             return "New User Created";
         }
         else return "User already present";
+    }
+
+    public static Integer getIdCount() {
+        return idCount;
+    }
+
+    public static void setIdCount(Integer idCount) {
+        NewRegister.idCount = idCount;
+    }
+
+    public static HashMap<Integer, User> getRegisteredUser() {
+        return registeredUser;
+    }
+
+    public static void setRegisteredUser(HashMap<Integer, User> registeredUser) {
+        NewRegister.registeredUser = registeredUser;
     }
 }
