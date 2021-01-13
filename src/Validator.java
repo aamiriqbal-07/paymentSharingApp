ee959818fd742812a930ea9d9b625cfc890a11ef
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-13 06:26:50 +0530

    Fixed Input user issue

--- src/Validator.java
+++ src/Validator.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class Validator {
 
     public static Boolean checkRegistry( HashMap<Integer, User> registeredUser, User user ) {
-        for(int i = 0; i< registeredUser.size() ; i++) {
+        for(int i = 1; i<= registeredUser.size() ; i++) {
             if( user.getPhone() == registeredUser.get(i).getPhone() ) return false;
         }
         return true;
